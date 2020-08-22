a6c3f4dda48ce63e9b5aecf9db9581375812dbaa
marco <marcodecrema@libero.it>
2020-08-22 18:18:45 +0200

    numero carrozza e codice CP

--- js/script.js
+++ js/script.js
@@ -7,10 +7,12 @@ btnGenera.addEventListener("click", function() {
   var passeggero = document.getElementById("passeggero");
   var offerta = document.getElementById("offerta");
   var carrozza = document.getElementById("carrozza");
-  var numeroCarrozza = Math.floor(Math.random * 10) + 1;
+  var numeroCarrozza = Math.floor(Math.random() * 10) + 1;
   var codice = document.getElementById("codice");
-  var codiceCp = Math.floor(Math.random * 10000) + 90000;
+  var codiceCp = Math.floor(Math.random() * 10000) + 90000;
   var costo = document.getElementById("costo");
+  var biglietto_container = document.getElementById("bigl_cont");
+  var dett_h3 = document.getElementById("dett_h3");
 
   var prezzoKm = 0.21;
   var totale = km * prezzoKm;
@@ -22,7 +24,7 @@ btnGenera.addEventListener("click", function() {
     offerta.innerHTML = "Sconto Minorenne";
   } else if (eta == 1) {
     totale = totale * 1;
-    offerta.innerHTML = "Offerta Standard";
+    offerta.innerHTML = "Biglietto Standard";
   } else if (eta == 2 ){
     var sconto = ((totale * 40) / 100);
     totale = totale - sconto;
@@ -30,6 +32,8 @@ btnGenera.addEventListener("click", function() {
   }
   totale = totale.toFixed(2);
 
+  biglietto_container.style.display = "block";
+  dett_h3.style.display = "block";
   passeggero.innerHTML = nome;
   carrozza.innerHTML = numeroCarrozza;
   codice.innerHTML = codiceCp;
