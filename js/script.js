e7a5eb87f323044001ae0439c122a7558716cb60
marco <marcodecrema@libero.it>
2020-08-22 17:58:57 +0200

    calcolo sconto e totale biglietto

---
+++ js/script.js
@@ -0,0 +1,39 @@
+var btnGenera = document.getElementById("genera");
+
+btnGenera.addEventListener("click", function() {
+  var nome = document.getElementById("nome").value;
+  var km = document.getElementById("km").value;
+  var eta = document.getElementById("eta").value;
+  var passeggero = document.getElementById("passeggero");
+  var offerta = document.getElementById("offerta");
+  var carrozza = document.getElementById("carrozza");
+  var numeroCarrozza = Math.floor(Math.random * 10) + 1;
+  var codice = document.getElementById("codice");
+  var codiceCp = Math.floor(Math.random * 10000) + 90000;
+  var costo = document.getElementById("costo");
+
+  var prezzoKm = 0.21;
+  var totale = km * prezzoKm;
+
+
+  if (eta == 0) {
+    var sconto = ((totale * 20) / 100);
+    totale = totale - sconto;
+    offerta.innerHTML = "Sconto Minorenne";
+  } else if (eta == 1) {
+    totale = totale * 1;
+    offerta.innerHTML = "Offerta Standard";
+  } else if (eta == 2 ){
+    var sconto = ((totale * 40) / 100);
+    totale = totale - sconto;
+    offerta.innerHTML = "Sconto Over-65";
+  }
+  totale = totale.toFixed(2);
+
+  passeggero.innerHTML = nome;
+  carrozza.innerHTML = numeroCarrozza;
+  codice.innerHTML = codiceCp;
+  costo.innerHTML = totale;
+
+
+});
